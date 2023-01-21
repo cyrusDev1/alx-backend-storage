818627d393fbd511af2798af18d2e59e2cdebbcb
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-01-21 17:51:22 +0100

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,10 +5,16 @@ client = MongoClient(host="localhost", port=27017)
 db = client.logs
 
 
-print("{} logs".format(db.nginx.count_documents({})))
-print("Methods:")
-for i in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    print("\tmethod {}: {}".format(
-        i, db.nginx.count_documents({"method": i})))
-print("{} status check".format(db.nginx.count_documents(
-    {"method": "GET", "path": "/status"})))
+def log_stats():
+    """print logs stats"""
+    print("{} logs".format(db.nginx.count_documents({})))
+    print("Methods:")
+    for i in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(
+            i, db.nginx.count_documents({"method": i})))
+    print("{} status check".format(db.nginx.count_documents(
+        {"method": "GET", "path": "/status"})))
+
+
+if __name__ == "__main__":
+    log_stats()
