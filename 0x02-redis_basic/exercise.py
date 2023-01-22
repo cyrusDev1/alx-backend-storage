8ee2ae9dc3f9e04b1a76ef1ea84e2be498757be0
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-01-22 15:30:02 +0100

    task 1 recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -17,3 +17,18 @@ class Cache:
         myuuid = str(uuid.uuid4())
         self._redis.set(myuuid, data)
         return myuuid
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
+        """used to convert the data back to the desired format."""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, data: str) -> str:
+        """get str"""
+        return data.decode('utf-8', 'strict')
+
+    def get_int(self, data: str) -> int:
+        """get int"""
+        return int(data)
